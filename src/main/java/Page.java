f2cb6f8218490fa6492ae62c1c9a81645b83596d
Dominik Huber <elcpt@elcpt.de>
2022-08-24 15:40:52 +0200

    Add Page class
    Class representing a single codebook page, final building block before first light can be assembled.

---
+++ src/main/java/Page.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Page {
+
+    private final byte[] randomBytes;
+    private static final String PAGE_PREFIX = "\\\\begin{flushleft}" 
+            + "\\\\begin{tabular}{c c c c c @{\\\\hskip 2em} c c c c c @{\\\\hskip 2em} c c c c c}";
+    private static final String PAGE_SUFFIX = "\\\\end{tabular}\n\\\\end{flushleft}\n\\\\newpage";
+
+    public Page(byte[] randomBytes) {
+        if (randomBytes.length < 150) {
+            throw new IllegalArgumentException("Need at least 150 random bytes for one page.");
+        }
+        this.randomBytes = randomBytes;
+    }
+
+    private String[] byteArrayToStringArray(byte[] arr) {
+
+        String[] result = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = Byte.toString(arr[i]);
+        }
+        return result;
+
+    }
+
+    @Override
+    public String toString() {
+        // TO LaTeX String
+        int arrayPointer = 0;
+        List<String> blocks = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            byte[] currentBytes = new byte[15];
+            for (int j = 0; j < 15; j++) {
+
+                currentBytes[j] = randomBytes[arrayPointer];
+                arrayPointer++;
+
+            }
+
+            String block = "\t" + String.join(" & ", byteArrayToStringArray(currentBytes)) + "\\\\"
+                    + "\\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U & \\\\U \\\\\\\\ \\\\\\\\";
+            blocks.add(block);
+        }
+
+        String blocksStr = String.join("\\hline \\\\\n", blocks);
+        return PAGE_PREFIX + "\n" + blocksStr + "\n" + PAGE_SUFFIX;
+
+    }
+}
--- src/main/java/TestingMain.java
+++ src/main/java/TestingMain.java
@@ -1,6 +1,12 @@
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TestingMain {
+
+    private static final RandomByteSource deviceReader = new DeviceByteReader("/dev/urandom");
+
     public static void main(String[] args) {
 
         // Test the trivial byte source
@@ -13,13 +19,37 @@ public class TestingMain {
         System.out.println("The second set of bytes is: " + Arrays.toString(bytes2));
 
         // Test the DeviceByteReader
-        RandomByteSource source2 = new DeviceByteReader("/dev/urandom");
-        byte[] bytes3 = source2.getBytes(8);
+        byte[] bytes3 = deviceReader.getBytes(8);
         System.out.println("The third set of bytes is: " + Arrays.toString(bytes3));
 
         // Testing bytes in range, this uses rejection sampling
-        byte[] bytes4 = source2.getBytesInRange(32, (byte) 0, (byte) 64);
+        byte[] bytes4 = deviceReader.getBytesInRange(32, (byte) 0, (byte) 64);
         System.out.println("The fourth set of bytes is: " + Arrays.toString(bytes4));
+
+        // Generate test pages and write them to disk
+        generateTestPages();
+
+    }
+
+    // Generate some test pages using the trivial source
+    private static void generateTestPages() {
+
+        List<String> pages = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            byte[] random = deviceReader.getBytesInRange(150, (byte) 0, (byte) 35);
+            Page p = new Page(random);
+            pages.add(p.toString());
+
+        }
+
+        String pagesStr = String.join("\n", pages);
+        try (PrintWriter out = new PrintWriter("pages.tex")) {
+
+            out.println(pagesStr);
+
+        } catch (Exception e) {
+            System.out.println("Exception, too bad! Msg.: " + e.getLocalizedMessage());
+        }
 
     }
 }
